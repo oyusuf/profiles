a44812979673231d5bef08da257066233b06ba18
root <root@datanetx.comm>
2016-01-23 13:45:43 -0500

    profiles edited

--- manifests/elkkibana.pp
+++ manifests/elkkibana.pp
@@ -1,3 +1,17 @@
 class profiles::elkkibana {
- include kibana4
+ include kibana4}
+class { '::kibana4':
+  package_ensure    => '4.3.0-linux-x64',
+  package_provider  => 'archive',
+  symlink           => true,
+  manage_user       => true,
+  kibana4_user      => kibana4,
+  kibana4_group     => kibana4,
+  kibana4_gid       => 507,
+  kibana4_uid       => 507,
+  config            => {
+      'server.port'           => 5601,
+      'server.host'           => '10.10.10.20',
+      'elasticsearch.url'     => 'http://10.10.10.20:9200',
+      }
 }
