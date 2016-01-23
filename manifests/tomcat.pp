8050b56e613174ddc196e4c9fbd1a371fe8607f4
root <root@datanetx.comm>
2016-01-23 15:08:37 -0500

    tomcat

---
+++ manifests/tomcat.pp
@@ -0,0 +1,15 @@
+class profiles::tomcat {
+  include tomcat} 
+class { 'tomcat':
+  install_from_source => false,
+}
+class { 'epel': }->
+tomcat::instance{ 'default':
+  package_name => 'tomcat',
+}->
+
+tomcat::service { 'default':
+  use_jsvc     => false,
+  use_init     => true,
+  service_name => 'tomcat',
+}
