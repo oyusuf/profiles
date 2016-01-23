0cc2bbd95b98e25f05edf2c0c2b9000112257823
root <root@datanetx.comm>
2016-01-23 16:41:02 -0500

    test

--- manifests/tomcat.pp
+++ manifests/tomcat.pp
@@ -1,7 +1,6 @@
 class profiles::tomcat {
   include tomcat}
 class { 'tomcat': }
-class { 'java': }
 tomcat::instance { 'test':
   source_url => 'http://mirror.nexcess.net/apache/tomcat/tomcat-8/v8.0.8/bin/apache-tomcat-8.0.8.tar.gz'
 }->
