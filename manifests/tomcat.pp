316d43933c2f20292ee0d55d6c626911a44b9f9f
gitadmin <yomlinux@gmail.com>
2016-01-26 12:57:39 -0500

    tomcat deleted

--- manifests/tomcat.pp
+++
@@ -1,7 +0,0 @@
-class profiles::tomcat {
-  include tomcat}
-class { 'tomcat': }
-tomcat::instance { 'test':
-  source_url => 'http://mirror.nexcess.net/apache/tomcat/tomcat-8/v8.0.8/bin/apache-tomcat-8.0.8.tar.gz'
-}->
-tomcat::service { 'default': } 
