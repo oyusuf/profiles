73f99c5b65b053c6e2d151f9540415c36f6150c6
admin <yomlinx@gmail.com>
2016-03-08 22:16:57 -0500

    test

--- manifests/httpd.pp
+++ manifests/httpd.pp
@@ -1,9 +1,8 @@
 class profiles::httpd {
-    class { 'apache':}
-
+  #  class { 'apache':}
+     include apache }
 apache::vhost { '10.10.10.18':
   port     => 80,
   docroot  => '/tmp/testvhost',
   template => 'apache/test.vhost.erb'
-  } 
-}
+  }
