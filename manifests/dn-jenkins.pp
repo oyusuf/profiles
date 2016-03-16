3f1418b5f220175d8199f056c6f04032ee0108de
admin <yomlinx@gmail.com>
2016-03-16 14:21:37 -0400

    jenkins code

--- manifests/dn-jenkins.pp
+++ manifests/dn-jenkins.pp
@@ -1,21 +1,22 @@
 class profile::dn-jenkins {
-  include jenkins::master
-
-  firewall { '500 allow Jenkins inbound traffic':
+node 'dn19.datanetx.comm' {
+   include jenkins
+   include jenkins::master
+}
+ firewall { '500 allow Jenkins inbound traffic':
     action => accept,
     dport  => 8081,
     proto  => tcp,
  }
   jenkins::user { 'oyusuf':
   email    => 'yomlinux@gmail.com',
-  password => 'password', 
-  ensure => 'present',
+  password => 'password',
+  ensure   => 'present',
  }
-   service { 'jenkins':
+  service { 'jenkins':
     ensure     => 'present',
-#    enable     => ,
+    enable     => true,
     hasstatus  => true,
     hasrestart => true,
   }
- 
 }
