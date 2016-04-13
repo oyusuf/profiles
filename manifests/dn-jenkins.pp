25b30e45e2a94e18395f728ab37f41a2e392b9d8
root <root@datanetx.comm>
2016-04-13 10:26:23 -0400

    added dn-jenkins module to puppet

--- manifests/dn-jenkins.pp
+++ manifests/dn-jenkins.pp
@@ -1,3 +1,10 @@
 class profiles::dn-jenkins {
    include jenkins
+
+  firewall { '100 allow access to jenkins':
+    ensure => 'present',
+    dport  =>   [8080],
+    proto  =>   tcp,
+    action =>  accept,
+ }
 }
