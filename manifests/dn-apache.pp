5ba997305d6265f48eb7cb0a70bfbea78ff80f6a
root <root@datanetx.comm>
2016-04-13 16:43:57 -0400

    modify dn-apache.pp class

--- manifests/dn-apache.pp
+++ manifests/dn-apache.pp
@@ -1,8 +1,9 @@
 class profiles::dn-apache {
-  #  class { 'apache':}
-     include apache }
+     include apache 
+
 apache::vhost { '10.10.10.18':
-  port     => 80,
-  docroot  => '/tmp/testvhost',
-  template => 'apache/test.vhost.erb'
+    port     => 80,
+    docroot  => '/tmp/testvhost',
+    template => 'apache/test.vhost.erb'
   }
+}
