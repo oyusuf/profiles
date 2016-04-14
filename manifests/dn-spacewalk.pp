d2f6e1fcec405a09fa01229ef9bb6743a0f789ab
root <root@datanetx.comm>
2016-04-14 15:25:26 -0400

    modified space walk

--- manifests/dn-spacewalk.pp
+++ manifests/dn-spacewalk.pp
@@ -1,7 +1,10 @@
 class profiles::dn-spacewalk {
   class { 'rhn_register':
-    sslca         => 'http://10.10.10.55/pub/RHN-ORG-TRUSTED-SSL-CERT',
-    serverurl     => 'http://10.10.10.55/XMLRPC',
+    sslca         => '/var/www/html/pub/RHN-ORG-TRUSTED-SSL-CERT',
+    serverurl     => 'http://10.10.10.55',
+    username      => 'oyusuf',
+    password      => '#$P@ssw0rd',
+    force         =>  true,
     activationkey => '1-d7c1e6d780491a52a24772a03d7f5797',
  }
 }
