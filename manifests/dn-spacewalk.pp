01b0e6eb6d1118044cd2ab3c9aadd8f307736421
root <root@datanetx.comm>
2016-04-14 15:40:29 -0400

    spacewalk

--- manifests/dn-spacewalk.pp
+++ manifests/dn-spacewalk.pp
@@ -2,9 +2,9 @@ class profiles::dn-spacewalk {
   class { 'rhn_register':
     sslca         => '/var/www/html/pub/RHN-ORG-TRUSTED-SSL-CERT',
     serverurl     => 'http://10.10.10.55',
-    username      => 'oyusuf',
-    password      => '#$P@ssw0rd',
-    force         =>  true,
+#    username      => 'oyusuf',
+#    password      => '#$P@ssw0rd',
+    force         =>  'true',
     activationkey => '1-d7c1e6d780491a52a24772a03d7f5797',
  }
 }
