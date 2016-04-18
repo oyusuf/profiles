96b3f9e1139e417e59c6da101204c2b0251b82fa
root <root@datanetx.comm>
2016-04-18 11:44:57 -0400

    firewall added

--- manifests/dn-spacewalk.pp
+++ manifests/dn-spacewalk.pp
@@ -8,3 +8,15 @@ class profiles::dn-spacewalk {
     activationkey => '1-d7c1e6d780491a52a24772a03d7f5797',
  }
 }
+
+
+class profiles::dn-spacewalk {
+class { 'rhn_register':
+  sslca         => '/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT',
+  serverurl     => 'http://10.10.10.55/XMLRPC',
+  activationkey => '1-1d6cfafaa90a972e7d3c2743e19f18cc',
+ }
+ class { 'rhn_register':
+    activationkey => '1-d7c1e6d780491a52a24772a03d7f5797',
+ }
+}
