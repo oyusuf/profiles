fe1f161b12367c9c088046ef3e5237d8cb6459c2
root <root@datanetx.comm>
2016-04-18 14:45:01 -0400

    spacewalk

--- manifests/dn-spacewalk.pp
+++ manifests/dn-spacewalk.pp
@@ -1,22 +1,14 @@
 class profiles::dn-spacewalk {
-   class { 'rhn_register':
-    sslca         => '/var/www/html/pub/RHN-ORG-TRUSTED-SSL-CERT',
-    serverurl     => 'http://10.10.10.55/XMLRPC',
-#    username      => 'oyusuf',
-#    password      => '#$P@ssw0rd',
-    force         =>  'true',
-    activationkey => '1-d7c1e6d780491a52a24772a03d7f5797',
- }
-}
+#class { 'rhn_register':
+#  sslca         => '/var/www/html/pub/RHN-ORG-TRUSTED-SSL-CERT',
+#  serverurl     => 'http://10.10.10.55/XMLRPC',
+#  activationkey => '1-d7c1e6d780491a52a24772a03d7f5797',
+# username      => 'oyusuf',
+# password      => '#$P@ssw0rd'
+# }
+# class { 'rhn_register':
+#    activationkey => '1-d7c1e6d780491a52a24772a03d7f5797',
+# }
+#}
 
-
-class profiles::dn-spacewalk {
-class { 'rhn_register':
-  sslca         => '/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT',
-  serverurl     => 'http://10.10.10.55/XMLRPC',
-  activationkey => '1-1d6cfafaa90a972e7d3c2743e19f18cc',
- }
- class { 'rhn_register':
-    activationkey => '1-d7c1e6d780491a52a24772a03d7f5797',
- }
-}
+  include spacewalk::client
