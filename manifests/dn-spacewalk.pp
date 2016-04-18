b5039b1f26fdb32f46a6c72ee1d10e9914b8bdd2
root <root@datanetx.comm>
2016-04-18 16:28:12 -0400

    spacewalk

--- manifests/dn-spacewalk.pp
+++ manifests/dn-spacewalk.pp
@@ -1,5 +1,5 @@
 class profiles::dn-spacewalk {
-#class { 'rhn_register':
+# class { 'rhn_register':
 #  sslca         => '/var/www/html/pub/RHN-ORG-TRUSTED-SSL-CERT',
 #  serverurl     => 'http://10.10.10.55/XMLRPC',
 #  activationkey => '1-d7c1e6d780491a52a24772a03d7f5797',
@@ -9,12 +9,12 @@ class profiles::dn-spacewalk {
 # class { 'rhn_register':
 #    activationkey => '1-d7c1e6d780491a52a24772a03d7f5797',
 # }
-#}
+# }
  class {'spacewalk':
   sslca         => '/var/www/html/pub/RHN-ORG-TRUSTED-SSL-CERT',
   serverurl     => 'http://10.10.10.55/XMLRPC',
   activationkey => '1-d7c1e6d780491a52a24772a03d7f5797',
 # username      => 'oyusuf',
 # password      => '#$P@ssw0rd'
+  }
 }
-} 
