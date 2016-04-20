ab7edef521891396a8328798242bfa07a0f7808c
root <root@datanetx.comm>
2016-04-20 12:15:57 -0400

    tomcat

--- manifests/dn-tomcat.pp
+++ manifests/dn-tomcat.pp
@@ -1,6 +1,10 @@
 class profiles::dn-tomcat {
   class { 'tomcat':}   
   include java
+
+ firewall { '100 allow access to tomcat':
+    ensure => 'present',
+    dport  =>   [8080],
+    proto  =>   tcp,
+    action =>  accept,
 }
-
-
