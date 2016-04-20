a8b8b631a42fe3caa3be07d3c170feeb2931f541
root <root@datanetx.comm>
2016-04-20 14:41:27 -0400

    tomcat

--- manifests/dn-tomcat.pp
+++ manifests/dn-tomcat.pp
@@ -2,10 +2,10 @@ class profiles::dn-tomcat {
   class { 'tomcat':
     install_from_source => false,
 }
-   class {'epel':}->
-   tomcat::instance {'default':
-     package_name => 'tomcat',
-}->
+#   class {'epel':}->
+#   tomcat::instance {'default':
+#     package_name => 'tomcat',
+#}->
    tomcat::service {'default':
      use_jsvc     => false,
      use_init     => true,
