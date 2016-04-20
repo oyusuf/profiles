44bcaf0dc137bfa2f2dece9cb92b05f8d56e50a2
root <root@datanetx.comm>
2016-04-20 10:35:35 -0400

    bash script added

--- manifests/dn-bash.pp
+++ manifests/dn-bash.pp
@@ -1,1 +1,16 @@
-class profiles::dn-bash
+class profiles::dn-bash {
+ file {'bash.sh':
+   ensure => 'file',
+   source => 'puppet:///modules/profiles/bash.sh',
+   path   => '/usr/local/bin/bash.sh',
+   owner  => 'root',
+   group  => 'root',
+   mode   => '0744',
+   notify => Exec['run_my_script'],
+ }
+ exec { 'run_my_script':
+  command => '/usr/local/bin/bash.sh',
+  refreshonly => true
+ }
+} 
+  
