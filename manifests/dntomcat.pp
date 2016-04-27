21c4ea558b91133ab92a4c5429dc94992577ee06
oyusuf <oyusuf@datanetx.comm>
2016-04-27 17:05:31 -0400

    test

--- manifests/dntomcat.pp
+++ manifests/dntomcat.pp
@@ -6,11 +6,13 @@ class { 'tomcat':
 }
 tomcat::instance { 'datanix1':
   server_control_port => 8005,
+  root_path           => '/home/tomcat/datanix1',
   http_port           => 8080,
   ajp_connector       => false,
 }
 tomcat::instance { 'datanix2':
   server_control_port => 8006,
+  root_path           => '/home/tomcat/datanix2',
   http_port           => 8081,
   manage_firewall     => true,
 }
