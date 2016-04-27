ba0300541c050c2c570cbf9bc8c663ab6494571a
oyusuf <oyusuf@datanetx.comm>
2016-04-27 16:26:27 -0400

    tomcat

--- manifests/dntomcat.pp
+++ manifests/dntomcat.pp
@@ -1,3 +1,19 @@
 class profiles::dntomcat {
-  include tomcat
+# include tomcat
+class { 'tomcat':
+  service_ensure => 'stopped',
+  service_enable => false
+}
+tomcat::instance { 'datanix1':
+  server_control_port => 8005,
+  http_port           => 8080,
+  ajp_connector       => false,
+  …
+}
+tomcat::instance { 'datanix2':
+  server_control_port => 8006,
+  http_port           => 8081,
+  manage_firewall     => true,
+  …
+}
 }
