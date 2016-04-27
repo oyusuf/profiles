557ee4fd9e16adce2744dda655fb92d3e1e14d02
oyusuf <oyusuf@datanetx.comm>
2016-04-27 16:04:45 -0400

    tomcat

--- manifests/dntomcat.pp
+++ manifests/dntomcat.pp
@@ -1,17 +1,3 @@
 class profiles::dntomcat {
-  class { 'tomcat': }
-class { 'java': }
-
-tomcat::instance { 'prod':
-  source_url => 'http://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.0/bin/apache-tomcat-8.5.0-deployer.tar.gz'
-}->
-tomcat::service { 'default': }
-
-
-  firewall { '100 allow access to tomcat':
-    ensure => 'present',
-    dport  =>   [8080],
-    proto  =>   tcp,
-    action =>  accept,
- }
+  include tomcat
 }
