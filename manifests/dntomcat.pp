785badaa974e220e71c9ace7d998e8b97f1a34da
oyusuf <oyusuf@datanetx.comm>
2016-04-27 16:30:43 -0400

    tomcat

--- manifests/dntomcat.pp
+++ manifests/dntomcat.pp
@@ -8,12 +8,10 @@ tomcat::instance { 'datanix1':
   server_control_port => 8005,
   http_port           => 8080,
   ajp_connector       => false,
-  …
 }
 tomcat::instance { 'datanix2':
   server_control_port => 8006,
   http_port           => 8081,
   manage_firewall     => true,
-  …
 }
 }
