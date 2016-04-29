3ec5f1179aa4569e2f9f9856f040beedf45c9b7c
oyusuf <oyusuf@datanetx.comm>
2016-04-29 01:30:36 -0400

    edited files

--- manifests/dncheckmk.pp
+++ manifests/dncheckmk.pp
@@ -2,11 +2,11 @@ class profiles::dncheckmk {
    package { 'check-mk-agent':
    ensure => present,
  }
-   file { '/etc/xinetd.d/check_mk':
+   file { '/etc/xinetd.d/check-mk-agent':
     owner   => 'root',
     group   => 'root',
     mode    =>  '0644',
-    source  =>  'puppet:///modules/profiles/check_mk',
+    source  =>  'puppet:///modules/profiles/check-mk-agent',
     require =>  Package['check-mk-agent'],
  }
 
