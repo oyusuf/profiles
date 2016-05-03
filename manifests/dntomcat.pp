74193f6db2a7dbe2d5639dbca8aaa08ec88fb1da
oyusuf <oyusuf@datanetx.comm>
2016-05-03 16:50:50 -0400

    added tomcat

--- manifests/dntomcat.pp
+++ manifests/dntomcat.pp
@@ -3,7 +3,7 @@ class profiles::dntomcat {
 
     tomcat::connector{'http-8080':
       ensure   => present,
-      instance => 'tomcat1',
+      instance => 'dn54.datanetx.comm',
       protocol => 'HTTP/1.1',
       port     => 8080,
       manage   => true,
@@ -11,17 +11,16 @@ class profiles::dntomcat {
 
     tomcat::connector{'ajp-8081':
       ensure   => present
-      instance => 'tomcat1'
+      instance => 'dn54.datanetx.comm'
       protocol => 'AJP/1.3',
       port     => 8081,
       manage   => true,
     }
 
-    tomcat::instance {'tomcat1':
+    tomcat::instance {'dn54.datanetx.comm':
       ensure    => present,
       group     => 'tomcat-admin',
       manage    => true,
       connector => ['http-8080','ajp-8081'],
     }
-
 }
