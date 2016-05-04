217077d55096d5ad778f5163f529c2cae1d0bf08
oyusuf <oyusuf@datanetx.comm>
2016-05-03 22:45:23 -0400

    dntomcat.pp

--- manifests/dntomcat.pp
+++ manifests/dntomcat.pp
@@ -1,5 +1,7 @@
 class profiles::dntomcat {
-{
+
+Exec {
+
   path => '/usr/bin:/usr/sbin/:/bin:/sbin:/usr/local/bin:/usr/local/sbin',
 }
 
@@ -9,5 +11,5 @@ tomcat::instance {'dn54.datanetx.comm':
   ensure    => present,
   http_port => '8080',
   http_address => '10.10.10.54',
-}
+ }  
 }
