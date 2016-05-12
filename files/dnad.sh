ffb6f1ff0ffbe115684f445df28a6a27c35e2af6
oyusuf <oyusuf@datanetx.comm>
2016-05-12 00:04:00 -0400

    updated

--- files/dnad.sh
+++ files/dnad.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 yum  install -y krb5-workstation sssd* samba samba-common authconfig authconfig-gtki ntp
-yum install -y samba-winbind samba-winbind-clients oddjob-mkhomedir pam_krb5 krb5-workstation 
-yum install -y oddjob oddjob-mkhomedir sssd* samba samba-common 
-yum install -y ldap* openldap* openldapclients openldapclient openldap-client openldap-clients   
+yum install -y samba-winbind samba-winbind-clients oddjob-mkhomedir pam_krb5 oddjob oddjob-mkhomedir sssd* samba samba-common 
+yum install -y ldap* openldap* openldapclients openldapclient openldap-client openldap-clients  
+authconfig --update --kickstart --enablewinbind --smbsecurity=ads --smbworkgroup=DATANETX --smbrealm=DATANETX.COMM
+--winbindtemplatehomedir=/home/%U --winbindtemplateshell=/bin/bash --enablewinbindusedefaultdomain --enablelocauthorize --enablekrb5 --krb5realm=DATANETX.COMM --enablekrb5kdcdns --enablekrb5realmdns --enablepamaccess
+net ads join -U Administrator 
