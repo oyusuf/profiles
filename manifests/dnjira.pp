e7f4b492dd7732f03f3abcbb04907993a87e7c4d
oyusuf <oyusuf@datanetx.comm>
2016-06-08 12:32:14 -0400

    Jira profiles download

---
+++ manifests/dnjira.pp
@@ -0,0 +1,31 @@
+class profiles::jira () {
+  # include jdk_oracle
+class { 'oracle_java':
+  version => '8u45',
+  type    => 'jdk'
+}
+
+  class { 'postgresql::server': }
+
+  postgresql::server::db { 'jira':
+    user     => 'jiraadm',
+    password => postgresql_password('jiraadm', 'mypassword'),
+  }
+
+  class { 'jira':
+    javahome => '/usr',
+    db       => 'postgresql',
+    dbuser   => 'jiraadm',
+  }
+
+  class { 'jira::facts':
+  }
+
+
+
+  firewall { '120 allow puppet stuff':
+    dport  => [8080],
+    proto  => tcp,
+    action => accept,
+  }
+}
