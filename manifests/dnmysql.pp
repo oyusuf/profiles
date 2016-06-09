14de3353d098745c0de73c5eedf66b20c8f68356
oyusuf <oyusuf@datanetx.comm>
2016-06-08 23:44:26 -0400

    test

--- manifests/dnmysql.pp
+++ manifests/dnmysql.pp
@@ -1,41 +1,27 @@
-#class profiles::dnmysql {
-# class {'mysql::server':
-#  root_password => 'password',
-#  remove_default_accounts => true,
-#  override_options        => $override_options
-# } 
-#}
-#class profiles::dnmysql (
-#  $override_options = hiera('mysql::override_options'),
-#  $db_user_name     = hiera('mysql::db_user_name'),
-#  $db_user_password = hiera('mysql::db_user_password'),
-#  $db_host          = hiera('mysql::db_host'),
-#  $db_name          = hiera('mysql::db_name'),
-#  $db_password      = hiera('mysql::db_password'),) {
-
 class profiles::dnmysql {
-  class { '::mysql::server':
-    root_password           => $db_password,
-    remove_default_accounts => true,
-  # override_options        => $override_options
-  }
 
-  mysql::db { $db_name:
-    user     => $db_user_name,
-    password => $db_user_password,
-    host     => $db_host,
-    grant    => ['SELECT', 'CREATE', 'DELETE', 'UPDATE'],
-  }
+class { 'mysql::server':
+  root_password => 'password'
+}
 
-  class { 'phpmyadmin':
-  }
-
-  phpmyadmin::server { 'default': }
+  mysql::db { 'dn53':
+   user     => 'root',
+   password => 'password',
+   host     => 'dn53.datanetx.comm',
+   grant    => ['SELECT', 'CREATE', 'DELETE', 'UPDATE'],
+}
+#mysql::db { "mydb_${fqdn}":
+#  user     => ',
+#  password => 'mypass',
+#  dbname   => 'mydb',
+#  host     => $::fqdn,
+#  grant    => ['SELECT', 'CREATE', 'DELETE', 'UPDATE'],
+#  tag      => $domain,
+#}
 
   firewall { '100 allow MySQL on 3306 & 3307':
     dport  => [3306, 3307, 80, 443],
     proto  => tcp,
     action => accept,
   }
-
 }
