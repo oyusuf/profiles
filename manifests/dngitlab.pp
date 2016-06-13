06227dc80e71db17a85662619d83d515bbddf40d
oyusuf <oyusuf@datanetx.comm>
2016-06-13 16:18:57 -0400

    github

--- manifests/dngitlab.pp
+++ manifests/dngitlab.pp
@@ -1,11 +1,11 @@
 class profiles::dngitlab () {
-  firewall { '120 allow puppet stuff':
+  firewall {'120 allow puppet stuff':
     dport  => [22, 80, 443],
     proto  => tcp,
     action => accept,
   }
 
-  class { '::gitlab':
+  class {'::gitlab':
     external_url => 'http://dn57.datanetx.comm',
   }
 
