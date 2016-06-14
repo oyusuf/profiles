eb82ff2cd1e0ea463b42acbecbbf050cb0385d05
oyusuf <oyusuf@datanetx.comm>
2016-06-13 23:33:37 -0400

    test

--- manifests/dnconfluence.pp
+++ manifests/dnconfluence.pp
@@ -1,10 +1,16 @@
-class profiles::dnconfluence () {
- 
- class {'mysql::server': } ->
-    mysql::server::db { 'confluencedb':
-    user     => 'confluenceuser',
-    password =>  password,
-  } ->
+class profiles::dnconfluence {
+
+class { 'mysql::server':
+  root_password => 'password'
+}
+
+  mysql::db { 'confluencedb':
+   user     => 'confluenceuser',
+   dbname   => 'confluencedb',
+   password => 'password',
+   host     => 'dn53.datanetx.comm',
+   grant    => ['ALL'],
+}   
  
  class {'oracle_java':
     version => '8u77',
