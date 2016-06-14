a199e4ea68d3640ee05dd4862af1f677fd803a81
oyusuf <oyusuf@datanetx.comm>
2016-06-13 21:53:58 -0400

    deploy

--- manifests/dndeploytar.pp
+++ manifests/dndeploytar.pp
@@ -1,17 +1,17 @@
 class profiles::dndeploytar ($source = '', $target = '') {
   include wget
 
-  wget::fetch {"download_some_tar_file":
-    source      => $source,
-    destination => $target,
-    timeout     => 0,
-    verbose     => false,
-  }
+#  wget::fetch {"download_some_tar_file":
+#    source      => $source,
+#    destination => $target,
+#    timeout     => 0,
+#    verbose     => false,
+#  }
 
-  exec {"untarit":
+  exec {"atlassian-jira-6.4.1.tar.gz":
     command => 'tar -xvf ',
     cwd     => $target,
     path    => ["/bin", "/usr/sbin"],
-    require => Exec['download_some_tar_file']
+#    require => Exec['download_some_tar_file']
   }
 }
