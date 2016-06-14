c7b697dd7d4b04da4c1a45cc633dbb516e89d800
oyusuf <oyusuf@datanetx.comm>
2016-06-13 23:23:10 -0400

    test

--- manifests/dnconfluence.pp
+++ manifests/dnconfluence.pp
@@ -1,15 +1,18 @@
 class profiles::dnconfluence () {
-  class { 'msqlsql::server': } ->
-  mysql::server::db { 'confluencedb':
+ 
+ class {'msqlsql::server': } ->
+    mysql::server::db { 'confluencedb':
     user     => 'confluenceuser',
     password =>  password,
   } ->
-  class { 'oracle_java':
+ 
+ class {'oracle_java':
     version => '8u77',
     type    => 'jdk'
   } ->
   file { ['/var/atlassian', '/var/atlassian/application-data', '/opt/atlassian',]: ensure => 'directory', } ->
-  class { 'confluence':
+ 
+ class {'confluence':
     version    => '5.9.6',
     installdir => '/opt/atlassian/confluence',
     homedir    => '/var/atlassian/application-data/confluence',
