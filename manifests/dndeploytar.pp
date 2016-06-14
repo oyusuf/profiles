f65dddcdbbe5ce035b789be632e8d1ad2afbc4f0
oyusuf <oyusuf@datanetx.comm>
2016-06-13 21:28:29 -0400

    deploytar

---
+++ manifests/dndeploytar.pp
@@ -0,0 +1,17 @@
+class profiles::deploytar ($source = '', $target = '') {
+  include wget
+
+  wget::fetch {"download_some_tar_file":
+    source      => $source,
+    destination => $target,
+    timeout     => 0,
+    verbose     => false,
+  }
+
+  exec {"untarit":
+    command => 'tar -xvf ',
+    cwd     => $target,
+    path    => ["/bin", "/usr/sbin"],
+    require => Exec['download_some_tar_file']
+  }
+}
