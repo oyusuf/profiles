7965cd765db91d2bbfe4a5ccb47166c34e5153b7
oyusuf <oyusuf@datanetx.comm>
2016-06-20 13:06:39 -0400

    con

--- manifests/dnconfluence.pp
+++ manifests/dnconfluence.pp
@@ -19,24 +19,25 @@ class { 'mysql::server':
   file { ['/var/atlassian', '/var/atlassian/application-data', '/opt/atlassian',]: ensure => 'directory', } ->
  
  class { 'confluence':
-    version    => '5.5.6',
+ #  version    => '5.5.6',
     deploy_module => 'archive',
     installdir => '/opt/atlassian/confluence',
     homedir    => '/var/atlassian/application-data/confluence',
     javahome   => '/usr/java/default',
   }
- file {'conf.sh':
-   ensure => 'file',
-   source => 'puppet:///modules/profiles/conf.sh',
-   path   => '/usr/local/bin/conf.sh',
-   owner  => 'root',
-   group  => 'root',
-   mode   => '0744',
-   notify => Exec['conf.sh'],
- }
 
- exec {'conf.sh':
-  command     => '/usr/local/bin/conf.sh',
-  refreshonly => true,
- }
+# file {'conf.sh':
+#   ensure => 'file',
+#   source => 'puppet:///modules/profiles/conf.sh',
+#   path   => '/usr/local/bin/conf.sh',
+#   owner  => 'root',
+#   group  => 'root',
+#   mode   => '0744',
+#   notify => Exec['conf.sh'],
+# }
+
+# exec {'conf.sh':
+#  command     => '/usr/local/bin/conf.sh',
+#  refreshonly => true,
+# }
 }
