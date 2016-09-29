ac3b7d8167f414551b89d7d60737cc2741cbd7c4
Joe <yomlinux@gmail.com>
2016-09-29 15:37:53 -0400

    kickstart

--- manifests/dnkickstart.pp
+++ manifests/dnkickstart.pp
@@ -1,10 +1,14 @@
 class profiles::kickstart {
- include kickstart
-   
-file { '/var/www/html/kickstart.cfg':
-    owner   => 'root',
-    group   => 'root',
-    mode     =>  '0644',
-    source   =>  'puppet:///modules/profiles/kickstart.cfg',
-  } 
+kickstart { '/var/www/html/kickstart.cfg':
+  commands   =>{
+
+},
+partition_configuration =>
 }
+# file { '/var/www/html/kickstart.cfg':
+#    owner   => 'root',
+#    group   => 'root',
+#    mode     =>  '0644',
+#    source   =>  'puppet:///modules/profiles/kickstart.cfg',
+#  } 
+
