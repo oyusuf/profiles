1a260e85fc179ecafbf146fda0e080526e5ec448
oyusuf <yomlinux@gmail.com>
2017-02-06 17:37:09 -0500

    test

--- manifests/dnresolv.pp
+++ manifests/dnresolv.pp
@@ -2,7 +2,7 @@ class profiles::dnresolv {
    file { '/etc/resolv.conf':
     owner   => 'root',
     group   => 'root',
-    mode    =>  '0644',
+    mode    =>  '0664',
     source  =>  'puppet:///modules/profiles/resolv.conf',
  }
 
