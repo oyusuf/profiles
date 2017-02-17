df20e56ca0b19c4c647fe80d20118698dc865ca7
oyusuf <yomlinux@gmail.com>
2017-02-16 21:21:43 -0500

    was

--- files/wasdmgr.sh
+++ files/wasdmgr.sh
@@ -3,10 +3,10 @@
 /opt/IBM/InstallationManager/eclipse/tools/imcl input /tmp/wassilent.xml -acceptLicense -sP
 
 ######################### Deployment Manger ( Edit to reflect your server name ###########################
-/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName Dmgr01 -appServerNodeName ustlvcmsp2343Node01 -enableService false -hostName ustlvcmsp2343.rdcms.eds.com -nodeName ustlvcmsp2343CellManager01 -cellName ustlvcmsp2343Cell01 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/cell/dmgr -profilePath /opt/IBM/WebSphere/AppServer/profiles/Dmgr01 -nodeProfilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv01 -enableAdminSecurity true -adminUserName wasadmin -adminPassword Stadia2810Thing
+/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName Dmgr01 -appServerNodeName dnx18Node01 -enableService false -hostName dnx18.datanix.comm -nodeName dnx18CellManager01 -cellName dnx18Cell01 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/cell/dmgr -profilePath /opt/IBM/WebSphere/AppServer/profiles/Dmgr01 -nodeProfilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv01 -enableAdminSecurity true -adminUserName wasadmin -adminPassword #$P@ssw0rd#$
 
 ######################## Was Profile Creations ################################
-/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName AppSrv01 -appServerNodeName ustlvcmsp2343Node01 -enableService false -hostname ustlvcmsp2343.rdcms.eds.com -nodeName ustlvcmsp2343CellManager01 -cellName ustlvcmsp2343Cell01 -dmgrProfilePath /opt/IBM/WebSphere/AppServer/profiles/Dmgr01  -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/default -profilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv01  -enableAdminSecurity true -adminUserName wasadmin -adminPassword Stadia2810Thing
+/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName AppSrv01 -appServerNodeName dnx18Node01 -enableService false -hostname dnx18.datanic.comm -nodeName dnx18CellManager01 -cellName dnx18Cell01 -dmgrProfilePath /opt/IBM/WebSphere/AppServer/profiles/Dmgr01  -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/default -profilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv01  -enableAdminSecurity true -adminUserName wasadmin -adminPassword #$P@ssw0rd#$
 
 ######################### Starting Dmgr  Server ################################
 /opt/IBM/WebSphere/AppServer/profiles/Dmgr01/bin/startManager.sh
