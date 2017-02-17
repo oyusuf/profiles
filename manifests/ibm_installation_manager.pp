f2a1cd28db818ea084fd59eb9f554c68e06df720
oyusuf <yomlinux@gmail.com>
2017-02-16 21:25:33 -0500

    db2

--- manifests/ibm_installation_manager.pp
+++ manifests/ibm_installation_manager.pp
@@ -1,7 +1,7 @@
 class profiles::ibm_installation_manager {
   class { 'ibm_installation_manager':
   deploy_source => true,
- source => 'http://ustlvcmsp2219.rdcms.eds.com/agent.installer.linux.gtk.x86_64_1.8.5000.20160506_1125.zip',
+ source => 'http://dnx13.datanix.comm/agent.installer.linux.gtk.x86_64_1.8.5000.20160506_1125.zip',
  #source => 'http://ustlvcmsp2199.rdcms.eds.com/agent.installer.linux.gtk.x86_1.8.5000.20160506_1125.zip',
   source_dir => '/opt/IBM/tmp/InstallationManager',
   target => '/opt/IBM/',
