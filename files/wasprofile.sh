5c8e44689eefe0387779959833a33ab657ec87cc
oyusuf <yomlinux@gmail.com>
2017-03-17 12:17:59 -0400

    AppSvr02

--- files/wasprofile.sh
+++ files/wasprofile.sh
@@ -3,15 +3,15 @@
 /opt/IBM/InstallationManager/eclipse/tools/imcl input /tmp/wassilent.xml -acceptLicense -sP
 
 ###################### Was profile for ordinary WAS Install ##############################################
-/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName AppSrv01 -enableService false -hostname dnx17.datanix.comm -serverName server1 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/default -profilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv01  -enableAdminSecurity true -adminUserName wasadmin -adminPassword Stadia2810Thing
+/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName AppSrv02 -enableService false -hostname dnx23.datanix.comm -serverName server1 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/default -profilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv02  -enableAdminSecurity true -adminUserName wasadmin -adminPassword FPS2Snippers#$
 
 ######################## Was Profile Creations for clauster WAS ###########################
 #/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName AppSrv01 -enableService false -hostname ustlvcmsp2344.rdcms.eds.com -nodeName ustlvcmsp2344Node01 -cellName ustlvcmsp2344Node01Cell -serverName server1 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/default -profilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv01  -enableAdminSecurity true -adminUserName wasadmin -adminPassword Stadia2810Thing
 
 #####################  Starting Was Server ###################################
-/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/bin/startNode.sh
-/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/bin/startServer.sh server1
+/opt/IBM/WebSphere/AppServer/profiles/AppSrv02/bin/startNode.sh
+/opt/IBM/WebSphere/AppServer/profiles/AppSrv02/bin/startServer.sh server1
 
 ####################  Was Federation ###################################
-#/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/bin/addNode.sh ustlvcmsp2343 8879 -username wasadmin -password Stadia2810Thing
+/opt/IBM/WebSphere/AppServer/profiles/AppSrv02/bin/addNode.sh dnx22.datanix.comm 8879 -username wasadmin -password FPS2Snippers
 
