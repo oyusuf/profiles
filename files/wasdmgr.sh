6c9141f0ff30d61b423cc60f2820b5ab87fd7137
oyusuf <yomlinux@gmail.com>
2017-03-17 10:56:38 -0400

    dmger

--- files/wasdmgr.sh
+++ files/wasdmgr.sh
@@ -3,15 +3,15 @@
 /opt/IBM/InstallationManager/eclipse/tools/imcl input /tmp/wassilent.xml -acceptLicense -sP
 
 ######################### Deployment Manger ( Edit to reflect your server name ###########################
-/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName Dmgr01 -appServerNodeName dnx18Node01 -enableService false -hostName dnx18.datanix.comm -nodeName dnx18CellManager01 -cellName dnx18Cell01 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/cell/dmgr -profilePath /opt/IBM/WebSphere/AppServer/profiles/Dmgr01 -nodeProfilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv01 -enableAdminSecurity true -adminUserName wasadmin -adminPassword #$P@ssw0rd#$
+/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName Dmgr01 -profilePath /opt/IBM/WebSphere/AppServer/profiles/Dmgr01 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/management -serverType DEPLOYMENT_MANAGER -cellName dnx22Cell01 -nodeName Dmgr01 -hostName dnx22.datanix.comm -enableAdminSecurity true -adminUserName wasadmin -adminPassword FPS2Snippers#$
 
 ######################## Was Profile Creations ################################
-/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName AppSrv01 -appServerNodeName dnx18Node01 -enableService false -hostname dnx18.datanic.comm -nodeName dnx18CellManager01 -cellName dnx18Cell01 -dmgrProfilePath /opt/IBM/WebSphere/AppServer/profiles/Dmgr01  -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/default -profilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv01  -enableAdminSecurity true -adminUserName wasadmin -adminPassword #$P@ssw0rd#$
+/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName AppSrv01 -profilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv01 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/default -serverName server1 -nodeName AppSrv01Node1 -hostName dnx22.datanix.comm -enableAdminSecurity true -adminUserName wasadmin -adminPassword FPS2Snippers#$
 
 ######################### Starting Dmgr  Server ################################
 /opt/IBM/WebSphere/AppServer/profiles/Dmgr01/bin/startManager.sh
 
-
 ###########################  Starting Was Server #############################
 /opt/IBM/WebSphere/AppServer/profiles/AppSrv01/bin/startNode.sh
 /opt/IBM/WebSphere/AppServer/profiles/AppSrv01/bin/startServer.sh server1
+
