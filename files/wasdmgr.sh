26d8591721f7399c54649f52336d21560c7505eb
oyusuf <yomlinux@gmail.com>
2017-03-17 13:03:45 -0400

    git

--- files/wasdmgr.sh
+++ files/wasdmgr.sh
@@ -6,7 +6,7 @@
 /opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName Dmgr01 -profilePath /opt/IBM/WebSphere/AppServer/profiles/Dmgr01 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/management -serverType DEPLOYMENT_MANAGER -cellName dnx22Cell01 -nodeName Dmgr01 -hostName dnx22.datanix.comm -enableAdminSecurity true -adminUserName wasadmin -adminPassword FPS2Snippers#$
 
 ######################## Was Profile Creations ################################
-/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName AppSrv01 -profilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv01 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/default -serverName server1 -nodeName AppSrv01Node1 -hostName dnx22.datanix.comm -enableAdminSecurity true -adminUserName wasadmin -adminPassword FPS2Snippers#$
+/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName AppSrv01  -enableService false -profilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv01 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/default -serverName server1 -nodeName AppSrv01Node1 -hostName dnx22.datanix.comm -enableAdminSecurity true -adminUserName wasadmin -adminPassword FPS2Snippers#$
 
 ######################### Starting Dmgr  Server ################################
 /opt/IBM/WebSphere/AppServer/profiles/Dmgr01/bin/startManager.sh
--- files/wasprofile.sh
+++ files/wasprofile.sh
@@ -3,14 +3,14 @@
 /opt/IBM/InstallationManager/eclipse/tools/imcl input /tmp/wassilent.xml -acceptLicense -sP
 
 ###################### Was profile for ordinary WAS Install ##############################################
-/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName AppSrv02 -enableService false -hostname dnx23.datanix.comm -serverName server1 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/default -profilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv02  -enableAdminSecurity true -adminUserName wasadmin -adminPassword FPS2Snippers#$
+/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName AppSrv02 -enableService false -hostname dnx23.datanix.comm -serverName server2 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/default -profilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv02  -enableAdminSecurity true -adminUserName wasadmin -adminPassword FPS2Snippers#$
 
 ######################## Was Profile Creations for clauster WAS ###########################
 #/opt/IBM/WebSphere/AppServer/bin/manageprofiles.sh -create -profileName AppSrv01 -enableService false -hostname ustlvcmsp2344.rdcms.eds.com -nodeName ustlvcmsp2344Node01 -cellName ustlvcmsp2344Node01Cell -serverName server1 -templatePath /opt/IBM/WebSphere/AppServer/profileTemplates/default -profilePath /opt/IBM/WebSphere/AppServer/profiles/AppSrv01  -enableAdminSecurity true -adminUserName wasadmin -adminPassword Stadia2810Thing
 
 #####################  Starting Was Server ###################################
 /opt/IBM/WebSphere/AppServer/profiles/AppSrv02/bin/startNode.sh
-/opt/IBM/WebSphere/AppServer/profiles/AppSrv02/bin/startServer.sh server1
+/opt/IBM/WebSphere/AppServer/profiles/AppSrv02/bin/startServer.sh server2
 
 ####################  Was Federation ###################################
 /opt/IBM/WebSphere/AppServer/profiles/AppSrv02/bin/addNode.sh dnx22.datanix.comm 8879 -username wasadmin -password FPS2Snippers#$
