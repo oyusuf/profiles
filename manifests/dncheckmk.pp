8e979ba395876d9c413238ea04c0a1a1766a4b26
oyusuf <yomlinux@gmail.com>
2017-03-21 11:36:48 -0400

    checkmk

--- manifests/dncheckmk.pp
+++ manifests/dncheckmk.pp
@@ -2,12 +2,16 @@ class profiles::dncheckmk {
    package { 'check-mk-agent':
    ensure => present,
  }
-   file { '/etc/xinetd.d/check-mk-agent':
+   package { 'epel-release':
+   ensure => present,
+ }
+  file { '/etc/xinetd.d/check-mk-agent':
     owner   => 'root',
     group   => 'root',
     mode    =>  '0644',
     source  =>  'puppet:///modules/profiles/check-mk-agent',
     require =>  Package['check-mk-agent'],
+    require =>  Package['epel-release'],
  }
 
 }
